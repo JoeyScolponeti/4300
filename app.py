bdde40aea04ac9936978157a9d5a16be0e619a6f
JoeyScolponeti <scolponeti.j@northeastern.edu>
2024-12-07 08:57:48 -0500

    file

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ st.header('Pro Mario Kart Wii Database')
 df = st.file_uploader(label= 'Upload Season', type=['csv'])
 
 if df:
-    df = pd.read_csv(df)
     output = reconstruct(df)
     st.write(output['tracks'])
 
