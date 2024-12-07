e8a738bc9d90e62f41e7a2d6594b6e64140bc5c7
JoeyScolponeti <scolponeti.j@northeastern.edu>
2024-12-07 10:01:39 -0500

    api

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from MANUAL import reconstruct
 from MKTBAPI import read
 import pandas as pd
 import csv
+import boto3
 
+s3_client = boto3.client('s3')
+
+bucket = "4300-final-bucket"
 
 st.header('Pro Mario Kart Wii Database')
 
@@ -14,5 +18,15 @@ if df:
     file_data = file_bytes.decode("utf-8").split('\n')
     matches, num = read(file_data)
     st.write(matches[0])
+
+    new_file = matches[0].name
+
+    with open (new_file, 'w') as f:
+        for match in matches:
+            f.write(match)
+
+    s3_client.upload_file(f, bucket, new_file)
+
+
 
 
