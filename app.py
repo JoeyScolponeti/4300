b0343fd0785d349c26c9564f8b718168b4928ec3
JoeyScolponeti <scolponeti.j@northeastern.edu>
2024-12-07 09:01:35 -0500

    file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from MANUAL import reconstruct
 import pandas as pd
+import csv
 
 
 
@@ -9,6 +10,7 @@ st.header('Pro Mario Kart Wii Database')
 df = st.file_uploader(label= 'Upload Season', type=['csv'])
 
 if df:
+    df = csv.reader(df)
     output = reconstruct(df)
     st.write(output['tracks'])
 
